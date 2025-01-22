2954bad5e8873def50775d4240a19fadca1b6cbd
Tunde <tchipsical@gmail.com>
2025-01-22 15:17:14 +0100

    More progress in user interface(1)

--- app/src/main/java/ng/com/sirteefyapps/tipcalc/MainActivity.kt
+++ app/src/main/java/ng/com/sirteefyapps/tipcalc/MainActivity.kt
@@ -1,6 +1,5 @@
 package ng.com.sirteefyapps.tipcalc
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,9 +13,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -27,11 +29,15 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -76,12 +82,17 @@ Column(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TipInputs() {
-    Surface(modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp, vertical = 10.dp), shape = RoundedCornerShape(10.dp)) {
-        Column() {
+    val inputText by remember { mutableStateOf("") }
+
+    Surface(modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp, vertical = 10.dp).border(width = 1.dp, color = Purple80), shape = RoundedCornerShape(10.dp)) {
+        Column {
             TextField(
                 leadingIcon = { Text("$", style = Typography.bodyLarge) },
-                value = "",
-                onValueChange = {},
+                value = inputText,
+                onValueChange = {
+
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                 label = { Text("Bill Amount", style = Typography.bodyLarge) },
                 modifier = Modifier.fillMaxWidth().padding(10.dp).border(
                     width = 1.dp,
@@ -94,13 +105,15 @@ fun TipInputs() {
                 )
             )
             VerticalSpacing(10)
-            Row(modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+            Row(modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
                 Text(
                     text = "Split",
                     style = Typography.bodyLarge,
                     modifier = Modifier.weight(1f)
                 )
                 SplitIcon(icon = Icons.Default.Add, onClick = {})
+                SplitIcon(icon = Icons.Default.ArrowDropDown, onClick = {})
+
             }
         }
     }
@@ -110,7 +123,7 @@ fun TipInputs() {
 fun SplitIcon(icon: ImageVector, onClick: () -> Unit) {
     Surface(
         modifier = Modifier.height(60.dp).width(60.dp).padding(10.dp),
-        shape = RoundedCornerShape(10.dp),
+        shape = CircleShape,
         color = Purple80,
         onClick = onClick
     ) {
